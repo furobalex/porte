8211ef827d948395c9c46fedbf75a80fcf0f682c
magik-boubou666 <magik-boubou666@hotmail.fr>
2012-07-24 11:01:20 +0200

    Ajout d'un bool pour savoir si les portes ont fini leur deplacement

--- porte.cpp
+++ porte.cpp
@@ -16,12 +16,12 @@ static unsigned long tpsPrec = 0;
 
 void ouvrirPorte()//la fonction qui ouvre les portes.
 {
-    pos = 180;
+    pos = POS_FIN;
 }
 
 void fermerPorte()//la fonction qui ferme les portes.
 {
-   pos = 0;
+   pos = POS_INIT;
 }
 
 // a ajouter a l'initialisation
@@ -36,6 +36,19 @@ void initialisation()
 void update()
 {
     tpsPrec = micros();
-    servoLeft.write(pos);
-    servoRight.write(pos);
+    servoLeft.write(pos-120);
+    servoRight.write(180-pos+90);
+}
+
+bool fini()
+{
+    if (micros() - tpsPrec < DELAI)
+    {
+        return false;
+    }
+    
+    else
+    {  
+        return true;
+    }
 }
--- porte.h
+++ porte.h
@@ -8,13 +8,17 @@
 
   #define PORTE
   
-  #define SERVOMOTOR_LEFT 10
-  #define SERVOMOTOR_RIGHT 9
+  #define SERVOMOTOR_LEFT 9
+  #define SERVOMOTOR_RIGHT 10
+  
+  #define POS_INIT 90
+  #define POS_FIN  190
   
   void ouvrirPorte();
   void fermerPorte();
   void initialisation();
   void update();
+  bool fini();
   
 #endif
   
